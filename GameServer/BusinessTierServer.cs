3017d1341ba8676efa4144e3ba62a0de81087fe2
Ryan Mackintosh <129054093+RJTLM@users.noreply.github.com>
2024-09-17 12:49:45 +0800

    File functionality issues resolved.

--- GameServer/BusinessTierServer.cs
+++ GameServer/BusinessTierServer.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using GameLobbyLib;
 
 namespace BusinessLayer
 {
@@ -14,19 +15,20 @@ namespace BusinessLayer
            
             //This should *definitely* be more descriptive.
             Console.WriteLine("Business Server starting...");
-            //This is the actual host service system
-            ServiceHost host;
-            //This represents a tcp/ip binding in the Windows network stack
-            NetTcpBinding tcp = new NetTcpBinding();
-            //Bind server to the implementation of BusinessServer
-            host = new ServiceHost(typeof(BusinessServerImplementation));
-            //Present the publicly accessible interface to the client. 0.0.0.0 tells .net to accept on any interface. :8100 means this will use port 8100. DataService is a name for the actual service, this can be any string.
+
+            // Use the BindingHelper to get the binding configuration
+            NetTcpBinding tcp = BindingHelper.CreateLargeFileBinding();
+
+            // Set up the host and bind the BusinessServerImplementation to the endpoint
+            ServiceHost host = new ServiceHost(typeof(BusinessServerImplementation));
             host.AddServiceEndpoint(typeof(IBusinessServerInterface), tcp, "net.tcp://0.0.0.0:8100/GameService");
-            //And open the host for business!
+
+            // Start the host
             host.Open();
             Console.WriteLine("Business Server Online");
             Console.ReadLine();
-            //Don't forget to close the host after you're done!
+
+            // Close the host after usage
             host.Close();
         }
     }
